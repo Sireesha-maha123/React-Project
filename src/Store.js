6c634d2b56b6c080e249a5702a318ba3f6964478
Sireesha-maha123 <sireesha.mahalakshmi@gmail.com>
2025-04-23 19:58:28 +0530

    added changes

--- src/Store.js
+++ src/Store.js
@@ -68,7 +68,10 @@ const productSlice= createSlice({
                         {image:"lattec.jpg",name:" Lettuce",price:60},
                         {image:"swiss.jpg",name:"Swiss Chard",price:30},
                         {image:"collared.jpg",name:"Collard Greens",price:40},
-                        {image:"mustord.jpg",name:"Mustard Greens",price:60}],
+                        {image:"mustord.jpg",name:"Mustard Greens",price:60},
+                        {image:"aru.jpg",name:"Arugula",price:40},
+                        {image:"aru.jpg",name:"Arugula",price:40},
+                        {image:"aru.jpg",name:"Arugula",price:40}],
 
             fruits:[
             {image:"banana.jpg",name:"Banana",price:80},,
